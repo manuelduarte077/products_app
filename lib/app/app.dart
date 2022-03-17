61e910f4edea837e9700f278529fdb4d9fe549c7
Manuel Duarte <carlosd99m@gmail.com>
2022-03-17 15:26:03 -0600

    project setup

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:product_app/screens/screens.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => const LoginScreen(),
+        'home': (context) => const HomeScreen(),
+      },
     );
   }
 }
