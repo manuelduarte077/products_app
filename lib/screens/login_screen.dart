b3e36c4f1f8482dbdabdb60eb1a8425934b58742
Manuel Duarte <carlosd99m@gmail.com>
2022-03-17 17:34:51 -0600

    validacion de los formularios

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:product_app/ui/input_decorations.dart';
 import 'package:product_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -13,26 +14,101 @@ class LoginScreen extends StatelessWidget {
             children: [
               const SizedBox(height: 250),
               CardContainer(
-                  child: Column(
-                children: [
-                  const SizedBox(height: 10),
-                  Text('Ingreso', style: Theme.of(context).textTheme.headline5),
-                  const SizedBox(height: 30),
-                  const Text('Formulario de login'),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      labelStyle: Theme.of(context).textTheme.bodyText1,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Text('Ingreso',
+                        style: Theme.of(context).textTheme.headline5),
+                    const SizedBox(height: 30),
+                    const _LoginForm(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              TextButton(
+                child: const Text(
+                  'Crear una cuenta nueva',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
                   ),
-                ],
-              )),
+                ),
+                onPressed: () {},
+              ),
+              const SizedBox(height: 50),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+              labelText: 'Correo electrónico',
+              hintText: 'manuel@gmail.com',
+              prefixIcon: Icons.alternate_email_sharp,
+            ),
+            validator: (value) {
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp = RegExp(pattern);
+
+              return regExp.hasMatch(value ?? '')
+                  ? null
+                  : 'El valor no parace un correo electrónico válido';
+            },
+          ),
+          const SizedBox(height: 30),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+              labelText: 'Contraseña',
+              hintText: '********',
+              prefixIcon: Icons.lock_outline,
+            ),
+            validator: (value) {
+              return (value != null && value.length >= 5)
+                  ? null
+                  : 'La contraseña debe tener al menos 6 caracteres';
+            },
+          ),
+          const SizedBox(height: 30),
+          MaterialButton(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            disabledColor: Colors.grey,
+            color: Colors.deepPurple,
+            elevation: 0,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+              child: const Text(
+                'Ingresar',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
